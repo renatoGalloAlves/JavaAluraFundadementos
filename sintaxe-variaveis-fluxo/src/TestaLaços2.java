80e9d15f1a516361bff740b1ff3f1a9a6ae8c7b6
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-10-20 11:04:16 -0300

    novos teste em laços

---
+++ sintaxe-variaveis-fluxo/src/TestaLaços2.java
@@ -0,0 +1,15 @@
+
+public class TestaLaços2 {
+	
+	public static void main(String[] args) {
+		for (int linha = 1; linha <10; linha++ ) {
+			for(int coluna = 0; coluna <= linha ; coluna++) {
+				//if (coluna > linha)
+					//break;
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
