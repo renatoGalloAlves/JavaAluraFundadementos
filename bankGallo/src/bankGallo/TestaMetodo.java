51d2b56062773809de1637bd69bbc9be032abbab
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-10-25 11:59:49 -0300

    metodos sacar, depositar, transferir

--- bankGallo/src/bankGallo/TestaMetodo.java
+++ bankGallo/src/bankGallo/TestaMetodo.java
@@ -4,9 +4,25 @@ public class TestaMetodo {
 
 	public static void main(String[] args) {
 		Conta contaDoRenato = new Conta();
-		contaDoRenato.saldo = 100;
-		contaDoRenato.deposita(50);
+			contaDoRenato.saldo = 100;
+			contaDoRenato.deposita(50);
 		System.out.println(contaDoRenato.saldo);
+		
+		contaDoRenato.saca(30);
+		System.out.println(contaDoRenato.saldo);
+		
+		Conta contaDaMarcela =new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		if(contaDaMarcela.transfere(300, contaDoRenato)) {
+			System.out.println("Transferencia com sucesso!");
+		}else {
+			System.out.println("Faltou dinheiro!");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoRenato.saldo);
+		
 	}
-
+	
+	
 }
