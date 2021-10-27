8333e7114251f122a7c808ad47a29167d68fb94a
renatoGalloAlves <renato_gallo_20@hotmail.com>
2021-10-27 15:33:46 -0300

    Novas intancias

---
+++ bankGallo-composto/src/testaBanco.java
@@ -0,0 +1,19 @@
+
+public class testaBanco {
+	
+	public static void main(String[] args) {
+		Cliente renato= new Cliente();
+		renato.nome= " Renato Alves";
+		renato.cpf = "111.222.333.66";
+		renato.profissao = "cozinheiro";
+		
+		Conta contaDoRenato= new Conta();
+		contaDoRenato.deposita(100);
+		
+		contaDoRenato.titular = renato;
+		System.out.println(contaDoRenato.titular);
+		
+		
+	}
+
+}
